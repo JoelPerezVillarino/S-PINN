a5744cd84e62b219e0b1ace4a4115753e003c4cd
JoelPerezVillarino <joel.perez.villarino@udc.es>
2022-09-19 13:52:26 +0200

    Codigo reestructurado

--- libs/ql_asset_valuator/__init__.py
+++
@@ -1,5 +0,0 @@
-from .vanilla_heston import VanillaHestonQL
-
-__all__ = [
-    "VanillaHestonQl"
-]
--- libs/stochastic_processes/__init__.py
+++
@@ -1,9 +0,0 @@
-from libs.stochastic_processes.processes2D import HestonProcess, LognormalProcess2D
-from libs.stochastic_processes.processes1D import LognormalProcess1D
-
-
-__all__ = [
-    "HestonProcess", 
-    "LognormalProcess2D",
-    "LognormalProcess1D"
-]
